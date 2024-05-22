ae0d639f50ea65d392d1cc00f5d195d7977aaa92
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-05-22 23:38:56 +0800

    update readme

--- lib/components/device_container.dart
+++ lib/components/device_container.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:pressureruvvi/functions/bluetooth_listener.dart';
+import 'package:pressureruvvi/home/data_listen.dart';
 
 class BluetoothDeviceContainer extends ConsumerWidget {
   final BluetoothDevice device;
   final bool connected;
-  final void Function()? onTap;
 
   const BluetoothDeviceContainer({
     super.key,
     required this.device,
     required this.connected,
-    required this.onTap,
   });
 
   @override
@@ -52,41 +52,86 @@ class BluetoothDeviceContainer extends ConsumerWidget {
                 ),
               ],
             ),
-            if (!connected)
-              InkWell(
-                onTap: onTap,
-                borderRadius: BorderRadius.circular(10),
-                child: IntrinsicWidth(
-                  child: Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 5,
-                      vertical: 4,
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 10,
-                      vertical: 5,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: device.isDisconnected
-                          ? const Color(0xFF313167)
-                          : const Color(0x00645aa4),
-                    ),
-                    child: device.isDisconnected
-                        ? const Text(
-                            'Connect',
-                            style: TextStyle(color: Colors.white, fontSize: 16),
-                          )
-                        : const Text(
-                            'View',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),
+            connected
+                ? InkWell(
+                    onTap: () async {
+                      print(
+                          "CONNECTED DEVICES INFO REDIRECTING TO DEVICE SCREEN");
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DeviceScreen(device: device),
+                        ),
+                      );
+                    },
+                    borderRadius: BorderRadius.circular(10),
+                    child: IntrinsicWidth(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 5,
+                          vertical: 4,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 5,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color(0x00645aa4),
+                        ),
+                        child: const Text(
+                          'View',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
                           ),
+                        ),
+                      ),
+                    ),
+                  )
+                : InkWell(
+                    onTap: () async {
+                      print(
+                          " CONNECTING TO DEVICE: ${device.advName} (${device.remoteId})");
+                      final isSuccess = await ref
+                          .read(bluetoothProviders)
+                          .connectToDevice(device);
+
+                      if (isSuccess) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Device Connected"),
+                          ),
+                        );
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Device Connection Failed"),
+                          ),
+                        );
+                      }
+                    },
+                    borderRadius: BorderRadius.circular(10),
+                    child: IntrinsicWidth(
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 5,
+                          vertical: 4,
+                        ),
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 10,
+                          vertical: 5,
+                        ),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0xFF313167)),
+                        child: const Text(
+                          'Connect',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              )
           ],
         ),
       ),
