11e836ab4bf069741f89859a499b22d6c06a0ccd
Mark Tristan R. Fabellar <marktristan.fabellar@cvsu.edu.ph>
2024-05-30 09:21:04 +0800

    added notification to a specific uuid

--- lib/home/data_listen.dart
+++ lib/home/data_listen.dart
@@ -1,5 +1,7 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 // class DeviceScreen extends StatelessWidget {
 //   final BluetoothDevice device;
@@ -55,18 +57,93 @@
 //   }
 // }
 
-import 'package:flutter/material.dart';
-import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+// class DeviceScreen extends StatefulWidget {
+//   final BluetoothDevice device;
 
-// class SensorData {
-//   final int timestamp;
-//   final double pressure1;
-//   final double pressure2;
-//   final double pressure3;
-//   final int pocketNumber;
+//   const DeviceScreen({super.key, required this.device});
 
-//   SensorData(this.timestamp, this.pressure1, this.pressure2, this.pressure3,
-//       this.pocketNumber);
+//   @override
+//   _DeviceScreenState createState() => _DeviceScreenState();
+// }
+
+// class _DeviceScreenState extends State<DeviceScreen> {
+//   List<BluetoothService> services = [];
+//   BluetoothCharacteristic? pressureCharacteristic;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _discoverServices();
+//   }
+
+//   Future<void> _discoverServices() async {
+//     List<BluetoothService> services = await widget.device.discoverServices();
+//     setState(() {
+//       this.services = services;
+//     });
+
+//     String pressureCharacteristicUUID =
+//         "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"; // Update this if needed
+
+//     for (var service in services) {
+//       for (var characteristic in service.characteristics) {
+//         if (characteristic.uuid.toString() == pressureCharacteristicUUID) {
+//           setState(() {
+//             pressureCharacteristic = characteristic;
+//           });
+//           await _startPressureNotifications();
+//           break;
+//         }
+//       }
+//     }
+
+//     if (pressureCharacteristic == null) {
+//       print('Pressure characteristic not found');
+//     }
+//   }
+
+//   Future<void> _startPressureNotifications() async {
+//     if (pressureCharacteristic != null &&
+//         pressureCharacteristic!.properties.notify) {
+//       await pressureCharacteristic!.setNotifyValue(true);
+//       pressureCharacteristic!.lastValueStream.listen((value) {
+//         // Parse and display the pressure value
+//         _handlePressureValue(value);
+//       });
+//     } else {
+//       print('Pressure characteristic not found or notifications not supported');
+//     }
+//   }
+
+//   void _handlePressureValue(List<int> value) {
+//     // Assuming the pressure is in bytes 5 and 6 (as a 16-bit unsigned integer in pascals)
+//     if (value.length >= 6) {
+//       int pressure = (value[4] << 8) | value[5];
+//       double pressureHpa = pressure / 100.0; // Convert to hPa if necessary
+
+//       print('Pressure value: $pressureHpa hPa');
+//     } else {
+//       print('Unexpected pressure value length: ${value.length}');
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.device.advName), // Change from advName to name
+//       ),
+//       body: ListView.builder(
+//         itemCount: services.length,
+//         itemBuilder: (context, index) {
+//           var service = services[index];
+//           return ListTile(
+//             title: Text(service.uuid.toString()),
+//           );
+//         },
+//       ),
+//     );
+//   }
 // }
 
 // class DeviceScreen extends StatefulWidget {
@@ -79,48 +156,59 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 // }
 
 // class _DeviceScreenState extends State<DeviceScreen> {
-//   List<SensorData> sensorDataList = [];
+//   List<BluetoothService> services = [];
+//   List<StreamSubscription<List<int>>> subscriptions = [];
 
 //   @override
 //   void initState() {
 //     super.initState();
-//     widget.device.connect();
-//     fetchSensorData();
+//     _discoverServices();
+//   }
+
+//   Future<void> _discoverServices() async {
+//     try {
+//       List<BluetoothService> services = await widget.device.discoverServices();
+//       setState(() {
+//         this.services = services;
+//       });
+
+//       for (var service in services) {
+//         for (var characteristic in service.characteristics) {
+//           // Subscribe to notifications if the characteristic supports it
+//           if (characteristic.properties.notify) {
+//             await characteristic.setNotifyValue(true);
+//             final subscription = characteristic.onValueReceived.listen((value) {
+//               print(
+//                   'Characteristic ${characteristic.uuid} value: $value'); // Handle notification value
+//             });
+
+//             subscriptions.add(subscription);
+//           }
+
+//           // Log the properties of each characteristic
+//           logCharacteristicProperties(characteristic);
+//         }
+//       }
+//     } catch (e) {
+//       print('Error discovering services: $e');
+//     }
+//   }
+
+//   void logCharacteristicProperties(BluetoothCharacteristic characteristic) {
+//     print('Characteristic ${characteristic.uuid}');
+//     print('Read: ${characteristic.properties.read}');
+//     print('Write: ${characteristic.properties.write}');
+//     print('Notify: ${characteristic.properties.notify}');
+//     // Add other properties as needed
 //   }
 
 //   @override
 //   void dispose() {
-//     widget.device.disconnect();
-//     super.dispose();
-//   }
-
-//   void fetchSensorData() async {
-//     List<BluetoothService> services = await widget.device.discoverServices();
-//     for (BluetoothService service in services) {
-//       for (BluetoothCharacteristic characteristic in service.characteristics) {
-//         if (characteristic.uuid.toString() ==
-//             '6E400003-B5A3-F393-E0A9-E50E24DCCA9E') {
-//           await characteristic.setNotifyValue(true);
-//           characteristic.lastValueStream.listen((value) {
-//             String dataString = String.fromCharCodes(value);
-//             SensorData sensorData = parseSensorData(dataString);
-//             setState(() {
-//               sensorDataList.add(sensorData);
-//             });
-//           });
-//         }
-//       }
+//     // Cancel all subscriptions when the widget is disposed
+//     for (var subscription in subscriptions) {
+//       subscription.cancel();
 //     }
-//   }
-
-//   SensorData parseSensorData(String dataString) {
-//     List<String> parts = dataString.split('\t');
-//     int timestamp = int.parse(parts[0]);
-//     double pressure1 = double.parse(parts[1]);
-//     double pressure2 = double.parse(parts[2]);
-//     double pressure3 = double.parse(parts[3]);
-//     int pocketNumber = int.parse(parts[4]);
-//     return SensorData(timestamp, pressure1, pressure2, pressure3, pocketNumber);
+//     super.dispose();
 //   }
 
 //   @override
@@ -129,27 +217,83 @@ import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 //       appBar: AppBar(
 //         title: Text(widget.device.advName),
 //       ),
-//       body: sensorDataList.isEmpty
-//           ? const Center(child: CircularProgressIndicator())
-//           : ListView.builder(
-//               itemCount: sensorDataList.length,
-//               itemBuilder: (context, index) {
-//                 final sensorData = sensorDataList[index];
-//                 return ListTile(
-//                   title: Text('Timestamp: ${sensorData.timestamp}'),
-//                   subtitle: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       Text('Pressure 1: ${sensorData.pressure1}'),
-//                       Text('Pressure 2: ${sensorData.pressure2}'),
-//                       Text('Pressure 3: ${sensorData.pressure3}'),
-//                       Text('Pocket Number: ${sensorData.pocketNumber}'),
-//                     ],
-//                   ),
-//                 );
-//               },
-//             ),
+//       body: FutureBuilder<List<BluetoothService>>(
+//         future: widget.device.discoverServices(),
+//         builder: (context, snapshot) {
+//           if (snapshot.connectionState == ConnectionState.waiting) {
+//             return const Center(child: CircularProgressIndicator());
+//           }
+
+//           if (snapshot.hasError) {
+//             return Center(child: Text('Error: ${snapshot.error}'));
+//           }
+
+//           return ListView(
+//             children: snapshot.data!.map((service) {
+//               return ExpansionTile(
+//                 title: Text(service.uuid.toString()),
+//                 children: service.characteristics.map((characteristic) {
+//                   return ListTile(
+//                     title: Text(characteristic.uuid.toString()),
+//                     subtitle: FutureBuilder<List<int>>(
+//                       future: _readCharacteristic(characteristic),
+//                       builder: (context, snapshot) {
+//                         if (snapshot.connectionState ==
+//                             ConnectionState.waiting) {
+//                           return const Text('Reading...');
+//                         }
+
+//                         if (snapshot.hasError) {
+//                           return Text('Error: ${snapshot.error}');
+//                         }
+
+//                         final value = snapshot.data;
+//                         return Text('Value: ${value.toString()}');
+//                       },
+//                     ),
+//                     trailing: IconButton(
+//                       icon: const Icon(Icons.download),
+//                       onPressed: () async {
+//                         if (characteristic.properties.read) {
+//                           try {
+//                             List<int> value = await characteristic.read();
+//                             print(
+//                                 'Characteristic ${characteristic.uuid} value: $value');
+//                             setState(
+//                                 () {}); // Force rebuild to show the updated value
+//                           } catch (e) {
+//                             print('Error reading characteristic: $e');
+//                           }
+//                         } else {
+//                           print(
+//                               'Characteristic ${characteristic.uuid} does not have read permission');
+//                         }
+//                       },
+//                     ),
+//                   );
+//                 }).toList(),
+//               );
+//             }).toList(),
+//           );
+//         },
+//       ),
 //     );
+//   }
+
+//   Future<List<int>> _readCharacteristic(
+//       BluetoothCharacteristic characteristic) async {
+//     if (characteristic.properties.read) {
+//       try {
+//         return await characteristic.read();
+//       } catch (e) {
+//         print('Error reading characteristic: $e');
+//         return [];
+//       }
+//     } else {
+//       print(
+//           'Characteristic ${characteristic.uuid} does not have read permission');
+//       return [];
+//     }
 //   }
 // }
 
@@ -164,7 +308,8 @@ class DeviceScreen extends StatefulWidget {
 
 class _DeviceScreenState extends State<DeviceScreen> {
   List<BluetoothService> services = [];
-  BluetoothCharacteristic? pressureCharacteristic;
+  Map<Guid, List<int>> characteristicValues = {};
+  List<StreamSubscription<List<int>>> subscriptions = [];
 
   @override
   void initState() {
@@ -173,39 +318,52 @@ class _DeviceScreenState extends State<DeviceScreen> {
   }
 
   Future<void> _discoverServices() async {
-    List<BluetoothService> services = await widget.device.discoverServices();
-    setState(() {
-      this.services = services;
-    });
+    try {
+      List<BluetoothService> services = await widget.device.discoverServices();
+      setState(() {
+        this.services = services;
+      });
 
-    // Assuming the pressure characteristic UUID is known
-    String pressureCharacteristicUUID = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
+      for (var service in services) {
+        for (var characteristic in service.characteristics) {
+          // Subscribe to notifications if the characteristic supports it
+          if (characteristic.properties.notify) {
+            await characteristic.setNotifyValue(true);
+            final subscription = characteristic.onValueReceived.listen((value) {
+              setState(() {
+                characteristicValues[characteristic.uuid] = value;
+              });
+              print(
+                  'Characteristic ${characteristic.uuid} value: $value'); // Handle notification value
+            });
 
-    for (var service in services) {
-      for (var characteristic in service.characteristics) {
-        if (characteristic.uuid.toString() == pressureCharacteristicUUID) {
-          setState(() {
-            pressureCharacteristic = characteristic;
-          });
-          _startPressureNotifications();
-          break;
+            subscriptions.add(subscription);
+          }
+
+          // Log the properties of each characteristic
+          logCharacteristicProperties(characteristic);
         }
       }
+    } catch (e) {
+      print('Error discovering services: $e');
     }
   }
 
-  void _startPressureNotifications() async {
-    if (pressureCharacteristic != null &&
-        pressureCharacteristic!.properties.notify) {
-      await pressureCharacteristic!.setNotifyValue(true);
-      pressureCharacteristic!.lastValueStream.listen((value) {
-        // Handle the pressure value
-        print('Pressure value: $value');
-        // Optionally update the UI with the pressure value
-      });
-    } else {
-      print('Pressure characteristic not found or notifications not supported');
+  void logCharacteristicProperties(BluetoothCharacteristic characteristic) {
+    print('Characteristic ${characteristic.uuid}');
+    print('Read: ${characteristic.properties.read}');
+    print('Write: ${characteristic.properties.write}');
+    print('Notify: ${characteristic.properties.notify}');
+    // Add other properties as needed
+  }
+
+  @override
+  void dispose() {
+    // Cancel all subscriptions when the widget is disposed
+    for (var subscription in subscriptions) {
+      subscription.cancel();
     }
+    super.dispose();
   }
 
   @override
@@ -214,34 +372,52 @@ class _DeviceScreenState extends State<DeviceScreen> {
       appBar: AppBar(
         title: Text(widget.device.advName),
       ),
-      body: services.isEmpty
-          ? const Center(child: CircularProgressIndicator())
-          : ListView(
-              children: services.map((service) {
-                return ListTile(
-                  title: Text(service.uuid.toString()),
-                  subtitle: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: service.characteristics.map((characteristic) {
-                      return ListTile(
-                        title: Text(characteristic.uuid.toString()),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.download),
-                          onPressed: () async {
-                            if (characteristic.properties.read) {
-                              List<int> value = await characteristic.read();
-                              print(
-                                  'Characteristic ${characteristic.uuid} value: $value');
-                              // You can also update the UI or do something with the value here
-                            }
-                          },
-                        ),
-                      );
-                    }).toList(),
-                  ),
-                );
-              }).toList(),
-            ),
+      body: FutureBuilder<List<BluetoothService>>(
+        future: widget.device.discoverServices(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          return ListView(
+            children: snapshot.data!.map((service) {
+              return ExpansionTile(
+                title: Text(service.uuid.toString()),
+                children: service.characteristics.map((characteristic) {
+                  return ListTile(
+                    title: Text(characteristic.uuid.toString()),
+                    subtitle: Text(
+                      'Value: ${characteristicValues[characteristic.uuid]?.toString() ?? 'Waiting for notification...'}',
+                    ),
+                    trailing: characteristic.properties.read
+                        ? IconButton(
+                            icon: const Icon(Icons.download),
+                            onPressed: () async {
+                              try {
+                                List<int> value = await characteristic.read();
+                                print(
+                                    'Characteristic ${characteristic.uuid} value: $value');
+                                setState(() {
+                                  characteristicValues[characteristic.uuid] =
+                                      value; // Update with the read value
+                                });
+                              } catch (e) {
+                                print('Error reading characteristic: $e');
+                              }
+                            },
+                          )
+                        : null,
+                  );
+                }).toList(),
+              );
+            }).toList(),
+          );
+        },
+      ),
     );
   }
 }
